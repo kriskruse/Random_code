6b87c512238e98900c4b58f8dbc13ea4140fcf7f
Kris Kruse <kriskruse@hotmail.com>
2022-10-30 11:20:39 +0100

    Update test.py

--- 02110 Algorithm and Data structures 2/test.py
+++ 02110 Algorithm and Data structures 2/test.py
@@ -1,11 +1,14 @@
-from itertools import  product
+from math import ceil
+from itertools import product
 
 
 def inFromCommand():
     return int(input()), list(map(int, input().split()))
 
+
 def takeGreedy(left, right):
     return "left" if left > right else "right"
+
 
 def updateList(action, cards):
     # Update the list
@@ -14,34 +17,35 @@ def updateList(action, cards):
         cards = cards[1:]
     else:
         score = cards[-1]
-        cards = cards[0:len(cards)-1]
+        cards = cards[0:len(cards) - 1]
     return score, cards
 
 
 def actionPhase(cards, action):
     score, cards = updateList(action, cards)
-    updateList(takeGreedy(cards[0],cards[-1]),cards)
+    if len(cards) < 1:
+        return score, cards
+    _, cards = updateList(takeGreedy(cards[0], cards[-1]), cards)
     return score, cards
 
-def mainGame(n,cards):
-    tempCards = cards
+
+def mainGame(n, cards):
+    originalCards = cards
     scoreList = []
-    algoScore = 0
     posActions = ["left", "right"]
-    actionList = list(product(posActions, repeat=int(n / 2)))
+    sampleLen = ceil(n / 2)
+    actionList = list(product(posActions, repeat=sampleLen))
 
     for actionPairs in actionList:
-        cards = tempCards
+        cards = originalCards
+        algoScore = 0
         for action in actionPairs:
             score, cards = actionPhase(cards, action)
             algoScore += score
         scoreList.append(algoScore)
-
     return max(scoreList)
 
 
 if __name__ == "__main__":
     n, cards = inFromCommand()
     print(mainGame(n, cards))
-
-
